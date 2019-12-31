613d65e448599d7850458548ef0585136b612768
Bryan Aguilar <b.aguilar4@gmail.com>
2019-12-30 21:17:20 -0800

    Polished whitespace

--- P5/P5p1.py
+++ P5/P5p1.py
@@ -19,16 +19,13 @@ def retrieveNum(mode,parameter):
 def execute():
     global input1
     i=0
-    while i < len(input1):
-        
+    while i < len(input1):       
         #inst  is our instruction
         #p1,p2,p3 will be our parameters modes
         # 0 means positions(retrieve data from that position)
         # 1 means immediate(that integer)
-        #opc will be our op code
-        
+        #opc will be our op code       
         inst = str(input1[i])
-        
         opc,p1,p2,p3 = 0,0,0,0
         opc = int(inst[-1])
         if(len(inst)>1):
@@ -40,24 +37,18 @@ def execute():
                 elif(j==3):
                     p2 = int(inst[-4])
                 elif(j==4):
-                    p3 = int(inst[-5]) 
-     
+                    p3 = int(inst[-5])    
         #if opcode is 1. This means addition of items i+1 and i+2 and store in location i +3
-       
         if(opc==1):
-            
-            
             input1[input1[i+3]]=retrieveNum(p1,input1[i+1])+retrieveNum(p2,input1[i+2])
             i+=4
         #checkopcode 2 same access and storage
         elif(opc==2):
-            #input1[input1[i+3]]=input1[input1[i+1]]*input1[input1[i+2]]
             input1[input1[i+3]]=retrieveNum(p1,input1[i+1])*retrieveNum(p2,input1[i+2])
             i+=4
         elif(opc==3):
             x = input("Please input the correct part ID")
             input1[input1[i+1]] = int(x)
-            
             i+=2
         elif(opc==4):
             if (p1==1):
@@ -72,7 +63,5 @@ def main():
     toInt() 
     execute()
     
-
-
 main()
   
